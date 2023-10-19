71aaa7597962fcb16a87c5ce766728b1c8cc9584
ramgopalio <ramgopal.metta@outlook.com>
2023-10-19 12:38:03 +0530

    sw-register

--- assets/js/main.js
+++ assets/js/main.js
@@ -290,16 +290,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
-
-// register the service worker
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            // Registration failed
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
